37e4f058d3b60e6faa3812c7a7a84245a4b70384
Mahmoud Zakaria <Ma7moudZakaria96@outlook.com>
2022-07-17 17:53:25 +0200

    Updated

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,41 +1,4 @@
-﻿using Flaminco.ProDownloader.Utilities;
-using Microsoft.Extensions.DependencyInjection;
-
-ServiceCollection services = new ServiceCollection();
-
-services.AddHttpClient<ResumableHttpClient>();
-services.AddTransient<ProfileDownloader>();
-services.AddTransient<ProfileCreator>();
-
-ServiceProvider provider = services.BuildServiceProvider();
-
-
-ProfileCreator profileCreator = provider.GetService<ProfileCreator>();
-
-
-profileCreator.Initializer("https://raw.githubusercontent.com/AhmedAbuelnour/MBs/master/4MB.txt", @"C:\Users\Ahmed\source\repos\Flaminco\ConsoleApp", "text.txt");
-
-
-var profiles = profileCreator.CreateSegmentProfilesAsync(8);
-
-
-List<ServerSegmentProfile> segments = new List<ServerSegmentProfile>();
-
-await foreach (var profile in profiles)
-{
-    ProfileDownloader ProfileDownloader = new ProfileDownloader();
-
-    segments.Add(profile);
-
-    await ProfileDownloader.DownloadAsync(profile, (e) =>
-    {
-        Console.WriteLine($"Current Percentage: {e.CurrentPercentage}");
-        Console.WriteLine($"Downloaded Progress: {e.DownloadedProgress}");
-        Console.WriteLine($"DownloadSpeed: {e.DownloadSpeed}");
-    });
-}
-
-await profileCreator.ReconstructSegmentProfilesAsync(segments);
+﻿using Microsoft.Extensions.DependencyInjection;
 
 //await fileProfileDownloader.DownloadAsync(profile, (e) =>
 //    {
